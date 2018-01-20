822cb6a3975ff8ef96dc8a5b2cfaf95c83b056ce
mariozef <zefirr@o2.pl>
2018-01-20 15:43:24 +0100

    Poprawa konfiguracji rollup

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,52 +1,19 @@
-
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default [{
-    input: 'src/scripts/login/jarc.ts',
+    input: 'out/index.js',
     output: {
-        name: 'jarc_login',
+        name: 'ts_rollup_babel',
         format: 'umd',
-        file: 'dist/login/jarc.bundle.es2015.js'
+        file: 'dist/index.bundle.es2015.js'
     },
     plugins: [
-        typescript2({
-            include: [ "login/*.ts+(|x)", "**/login/*.ts+(|x)" ],
-            "useTsconfigDeclarationDir": true,
-        }),
         resolve({
             jsnext: true,
             main: true,
             browser: true
         }),
-        commonjs(),
-        replace({
-            exclude: 'node_modules/**',
-            ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
-        }),
-        (process.env.NODE_ENV === 'production' && uglify())
-    ]
-},
-{
-    input: 'src/scripts/main/jarc.ts',
-    output: {
-        name: 'jarc_main',
-        format: 'umd',
-        file: 'dist/main/jarc.bundle.es2015.js'
-    },
-    plugins: [
-        typescript2({
-            include: [ "main/*.ts+(|x)", "**/main/*.ts+(|x)" ],
-            "useTsconfigDeclarationDir": true,
-        }),
-        resolve({
-            jsnext: true,
-            main: true,
-            browser: true
-        }),
-        commonjs(),
-        replace({
-            exclude: 'node_modules/**',
-            ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
-        }),
-        (process.env.NODE_ENV === 'production' && uglify())
+        commonjs()
     ]
 }]
